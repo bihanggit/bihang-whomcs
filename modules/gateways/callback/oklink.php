033fd90375f096b2f9fc451743a849654a1de103
liu tao <star2ko@gmail.com>
2015-05-02 20:33:29 +0800

    no message

--- modules/gateways/callback/oklink.php
+++ modules/gateways/callback/oklink.php
@@ -2,12 +2,12 @@
 
 
 # Required File Includes
-include '../../../dbconnect.php';
-include '../../../includes/functions.php';
-include '../../../includes/gatewayfunctions.php';
-include '../../../includes/invoicefunctions.php';
+include './../../../dbconnect.php';
+include './../../../includes/functions.php';
+include './../../../includes/gatewayfunctions.php';
+include './../../../includes/invoicefunctions.php';
 
-require_once '../oklink/lib/Oklink.php';
+require_once './../oklink/lib/Oklink.php';
 
 $gatewaymodule = "oklink";
 $GATEWAY       = getGatewayVariables($gatewaymodule);
@@ -21,7 +21,7 @@ $client = Oklink::withApiKey($GATEWAY['apiKey'], $GATEWAY['apiSecret']);
 if ( $client->checkCallback() ){
     $response = json_decode(file_get_contents('php://input'));
     # Checks invoice ID is a valid invoice number or ends processing
-    $invoiceid = $response->cumtom;
+    $invoiceid = $response->custom;
 
     $transid = $response->id;
     checkCbTransID($transid); # Checks transaction number isn't already in the database and ends processing if it does
